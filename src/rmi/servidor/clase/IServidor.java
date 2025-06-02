8b8bca3ca5419a091da5c52bc03c5aa10a0b8362
SolarSpectre <josephcaza32@gmail.com>
2025-06-01 20:15:02 -0500

    eliminar comentarios

--- src/rmi/servidor/clase/IServidor.java
+++ src/rmi/servidor/clase/IServidor.java
@@ -5,16 +5,12 @@ import java.rmi.RemoteException;
 import java.util.List;
 
 public interface IServidor extends Remote {
-    // Create
     boolean crear(Persona persona) throws RemoteException;
     
-    // Read
     Persona leer(int id) throws RemoteException;
     List<Persona> leerTodos() throws RemoteException;
     
-    // Update
     boolean actualizar(Persona persona) throws RemoteException;
     
-    // Delete
     boolean eliminar(int id) throws RemoteException;
 } 
